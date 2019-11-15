cc603f66b2c7407d3f2360f0e3433d99c4ce1918
Gopal Krishna <mydigitalanjel@gmail.com>
2019-11-16 01:42:54 +0530

    finished code for project 1

---
+++ 2. Value-Based Methods/p1_navigation/model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, fc_units=64, seed=0):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.fc1 = nn.Linear(state_size, fc_units)
+        self.fc2 = nn.Linear(fc_units, fc_units*2)
+        self.fc3 = nn.Linear(fc_units*2, fc_units*4)
+        self.output = nn.Linear(fc_units*4, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        
+        return self.output(x)
